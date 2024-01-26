acfc7fe6b646cd4364c29734adb7c6439ab29960
Yu Zhengwen <yuzhengwen0217@gmail.com>
2024-01-26 23:59:00 +0800

    Basic movement and hexagonal tilemap
    - Made tiles go red when the player walks over it for now
    - Very simple player controller for now

--- Assets/TilesManager.cs
+++ Assets/TilesManager.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TilesManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Tilemap tilemap;
+    private void Awake()
     {
-        
+        tilemap = GetComponent<Tilemap>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void destroyTile(Vector3Int pos)
+    {
+        Tile tile = tilemap.GetTile<Tile>(pos);
+        if (tile != null)
+        {
+            tilemap.SetTile(pos, null);
+        }
     }
 }
